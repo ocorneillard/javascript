70495dd2a54bce6b026398967132d7c19124e73e
ocorneillard <dorian.c.collier@gmail.com>
2018-04-25 12:20:14 +0200

    rand color

--- 11_keyboard/app.js
+++ 11_keyboard/app.js
@@ -1,1 +1,94 @@
-cccc
+
+let character = document.getElementById("character");
+
+  let keyPressed = function (e) {
+    let key = e.which || e.keyCode;
+    let rand = Math.floor(Math.random() * 255);
+    
+    switch (key) {
+      case 96:
+        character.style.backgroundColor = `rgba(
+          ${rand},
+          ${Math.floor(Math.random() * 255)},
+          ${Math.floor(Math.random() * 255)},
+          1)`;
+
+        break;
+      
+      case 97:
+        character.style.backgroundColor = `rgba(
+          ${rand},
+          ${Math.floor(Math.random() * 255)},
+          ${Math.floor(Math.random() * 255)},
+          1)`;
+        break;
+
+       case 98:
+       character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 99:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 100:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 101:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 102:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 103:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 104:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+
+      case 105:
+      character.style.backgroundColor = `rgba(
+        ${rand},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        1)`;
+        break;
+    }
+  }
+  window.addEventListener('keydown', keyPressed);
+
+  
+  
