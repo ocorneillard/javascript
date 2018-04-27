63fc6fb2e4371e431fdc1a2894f009cefd9b51d7
ocorneillard <dorian.c.collier@gmail.com>
2018-04-27 16:04:27 +0200

    add interval

--- clicker/assets/app.js
+++ clicker/assets/app.js
@@ -2,25 +2,38 @@ let affichage = document.querySelector(".affichage");
 let clickH = document.querySelector(".click");
 let affichageMultiply = document.querySelector(".multiply");
 let multiplicator = document.querySelector(".multiplicator");
-let score = 0;
+let autoclicker = document.querySelector(".autoclicker");
+
+let score = 500;
 let multiple = 1;
 let click = 1;
+let auto;
 
 function add(e) {
   score = score + (click * multiple);
-  affichage.innerHTML = score;
 }
 
 function multiply(e) {
-  if (score > 50) {
+  if (score > (50 * multiple)) {
+  score = score - (50 * multiple);
   multiple++;
-  multiplicator.innerHTML = `Multiplicator x${multiple}`;
+  multiplicator.innerHTML = `Multiplicator x${multiple} Cost for upgrade : ${(50*multiple)}`;
   affichageMultiply.innerHTML = multiple;
-  score = score - 50;
-  } else {
+  } 
+}
 
-  }
+function autoclick(e) {
+  if (score > 500) {
+    score++;
+  } 
+}
+
+function affiche() {
+  affichage.innerHTML = score;
 }
 
 clickH.addEventListener("click", add);
 multiplicator.addEventListener("click", multiply);
+autoclicker.addEventListener("click", autoclick);
+auto = setInterval(autoclick, 1000);
+let reload = setInterval(affiche, 10);
