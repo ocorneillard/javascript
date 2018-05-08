e7393c4d39f91778e34ac4c500af5cc9f0384ca3
ocorneillard <dorian.c.collier@gmail.com>
2018-05-08 11:40:54 +0200

    ex3

--- vanilla/ex2/ex2/app.js
+++ vanilla/ex2/ex2/app.js
@@ -1,6 +1,16 @@
 const img = document.querySelector('img');
-
-
 img.addEventListener("dblclick", function(){
   img.style.width = "500px";
+});
+
+const displayOn = document.getElementById('afficher');
+const displayOff = document.getElementById('cacher');
+const text = document.getElementById('texte');
+
+displayOn.addEventListener('click', function(e) {
+  text.style.display = '';
+});
+
+displayOff.addEventListener('click', function(e) {
+  text.style.display = 'none';
 });
