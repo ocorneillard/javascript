db72f58ff00a731f36d4bb73a7b2a1fe8f899b8f
ocorneillard <dorian.c.collier@gmail.com>
2018-05-11 15:33:55 +0200

    modify ex7

--- vanilla/ex2/ex7/app.js
+++ vanilla/ex2/ex7/app.js
@@ -31,11 +31,70 @@ bigger.addEventListener('click', (e) => {
     });
   /* souligner ou remettre en normal les mots en rouge */
 
-  
+  const redText = document.querySelectorAll('.rouge');
+  const highlighted = document.querySelector('#instructions li:nth-child(4)');
+
+  highlighted.addEventListener('click', (e) => {
+      redText.forEach((span) => {
+
+        if (span.style.textDecoration === "underline" ) {
+          span.style.textDecoration = "none";
+        } else {
+            span.style.textDecoration = "underline";
+        }
+      });
+    });
+
   /* remplacer le logo par une autre image au survol */
   
-  
+  const changeLogo = document.querySelector("img");
+  const img = document.querySelector("#instructions li:nth-child(5)");
+  img.addEventListener('mouseover', (e) => {
+      changeLogo.src = "koala.jpg";
+  });
   /* Afficher l'URL des liens dans une infobulle lors du survol des liens */
   
+  const url = document.querySelectorAll('a');
+
+  url.forEach((link) => {
+    link.addEventListener('mouseenter', (e) => {
+    let info = document.createElement('span');
+    info.className = 'infobulle';
+    info.innerHTML = link.href;
+    insertAfter(info,link);
+    });
+
+    link.addEventListener('mouseleave', (e) => {
+      const infobulle = document.querySelector('.infobulle');
+      if (infobulle !== null) {
+        infobulle.remove(infobulle);
+      }
+    });
+});
+
+
   /* ajouter "Chapitre 1 :" devant le 1er titre h2<br>et "Chapitre 2 :" devant le 2ème titre h2 */
+
+  const modh2 = document.querySelectorAll('h2')
+  let a = 1;
+  let b = 0;
+  const chap = document.querySelector('#instructions li:nth-child(7)');
   
+  chap.addEventListener('click', (e) => {
+    if (b === 0) {
+    modh2.forEach((h2) => {
+      let text = h2.textContent;
+      h2.innerText = `Chapitre ${a} : ${h2.textContent}`;
+      a++;
+      b = 1;
+    });
+    }
+  });
+
+
+
+
+
+  function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
