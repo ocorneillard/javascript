3aca08e8274fa333759c257cc9a8566725b9494a
ocorneillard <dorian.c.collier@gmail.com>
2018-05-14 11:34:19 +0200

    ex3 2

--- vanilla/ex3/app.js
+++ vanilla/ex3/app.js
@@ -1,8 +1,26 @@
 const input = document.querySelector('.input input');
-const button = document.querySelector('button');
+const button = document.querySelector('.subex1');
 // console.log(parseInt(input.value));
 button.addEventListener('click', (e) => {
   let value = parseInt(input.value);
   value++;
   input.value = value;
+});
+
+
+const subPlus = document.querySelector('.subplus');
+const subMinus = document.querySelector('.subminus');
+const displayTwo = document.querySelector('#plus');
+console.log(displayTwo)
+
+subPlus.addEventListener('click', (e) => {
+  let value = parseInt(displayTwo.value);
+  value++;
+  displayTwo.value = value;
+});
+
+subMinus.addEventListener('click', (e) => {
+  let value = parseInt(displayTwo.value);
+  value--;
+  displayTwo.value = value;
 });
